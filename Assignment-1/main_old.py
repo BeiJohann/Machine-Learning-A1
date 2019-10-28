693eba0326e1f72f5de95884391cc733de5a0ea9
Johannes Hegselmann <j_hegs01@wwu.de>
2019-10-28 16:37:27 +0100

    some testing

--- Assignment-1/main_old.py
+++ Assignment-1/main_old.py
@@ -11,7 +11,40 @@ from torch.autograd import Variable
 
 from datenloader import open_data
 
-open_data
+'''
+# The Real RNN, but it doesnt work
+class GRUNet(nn.Module):
+    def __init__(self, vocab_size, input_size, hidden_size, num_layers, output_size, dropout=0.01):
+        super().__init__()
+        self.embed = nn.Embedding(vocab_size, input_size)
+        self.gru = nn.GRU(input_size, hidden_size, num_layers=num_layers, dropout=dropout)
+        self.lin1 = nn.Linear(hidden_size, output_size)
+
+        self.num_layers = num_layers
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+
+    def forward(self, sequence):
+        hidden_layer = self.init_hidden(len(sequence[0]))
+        #print(sequence.size(),'1')
+        output = self.embed(sequence)
+        #print(output.size(),'2')
+        output, _ = self.gru(output, hidden_layer)
+        #print(output.size(),'3')
+        #take the take the final row of the middle dim. reduce it with squeez
+        #output = output.contiguous().view(-1, self.hidden_size * len(sequence[0]))
+        output = output[:,len(sequence[0])-1:,:]
+        output = output.squeeze(dim=1)
+        #print(output.size(),'4')
+        output = self.lin1(output)
+        return output
+
+    def set_dev(self, dev):
+        self.dev = dev
+
+    def init_hidden(self, seq_len):
+        return torch.zeros(self.num_layers, seq_len, self.hidden_size).float().to(self.dev)
+'''
 
 # from datenloader import get_numeric_representations_sents
 
